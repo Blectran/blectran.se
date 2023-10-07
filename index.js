3ba17c12e45ef067381f924f369bab895d1c6e8f
Blectran <79946820+Blectran@users.noreply.github.com>
2023-10-07 17:11:58 +0200

    added currently listening box, based upon lastfm

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-/*window.onscroll = function() {myFunction()};
+async function getCurrentSong() {
+    const lastFM = "https://blectran.se/lastsong.api.exe";
+    var response = await fetch(lastFM)
+    var json = await response.json();
+    console.log(json)
+    var song_data = json.recenttracks.track[0];
+    var artist = song_data.artist["#text"]
+    var song_name = song_data.name;
+    var song_image = song_data.image[1]["#text"];
+    
+    document.getElementById("song-title").innerText = song_name;
+    document.getElementById("song-artist").innerText = artist;
+    document.getElementById("song-image").src = song_image;
+}
 
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
-
-function myFunction() {
-  if (window.scrollY >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}*/
+getCurrentSong();
